dadb9abaa020931717cb6d34b60609359fad9935
jie.tan <jie.tan@77hub.com>
2021-10-31 10:29:05 +0000

    平台基本设置改名

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-31 06:40:33
+ * @LastEditTime: 2021-10-31 07:57:37
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /front-rdms/config/defaultSettings.ts
+ */
 import { Settings as LayoutSettings } from '@ant-design/pro-layout';
 
 const Settings: LayoutSettings & {
@@ -12,7 +20,7 @@ const Settings: LayoutSettings & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: 'R & D management platform',
   pwa: false,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
